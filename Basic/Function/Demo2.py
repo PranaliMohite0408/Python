98cb3250934fcf916ad4b396e70f5766e09708f1
unknown <pranalimohite482000@gmail.com>
2022-01-05 21:30:24 +0530

    Python Program

---
+++ Basic/Function/Demo2.py
@@ -0,0 +1,13 @@
+def fun(): 								#Function Defination
+	print("Inside fun Function....")
+	
+def main():								
+	print("Inside main Function....")
+	
+	fun()								#Function Call
+	
+if __name__ == "__main__":
+	main()
+    
+print("End Application.....")			
+	
