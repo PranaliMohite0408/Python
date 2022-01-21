c029a7c17825616cac9b57c8be0b1aa378510e73
unknown <pranalimohite482000@gmail.com>
2022-01-21 18:26:24 +0530

    Python Program

---
+++ Python Programs/Datatypes/02_Datatypes.py
@@ -0,0 +1,17 @@
+No = 12;
+
+Name = "XYZ";
+
+Marks = 90.32;
+
+print(No);             #12
+
+print(Name);           #XYZ
+
+print(Marks);          #90.32
+
+print(type(No));       #<class 'int'>
+
+print(type(Name));     #<class 'str'>
+
+print(type(Marks));    #<class 'float'>
