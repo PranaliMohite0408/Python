01d7ef2a62fe196299427ed03425287bbbf9f2f2
unknown <pranalimohite482000@gmail.com>
2022-01-23 10:41:41 +0530

    Python Program

---
+++ Python Programs/Tuple/01_Tuple.py
@@ -0,0 +1,28 @@
+#Tuple
+#Heterogeneous
+#Indexed
+#Sequencial
+#Ordered
+#Allows Duplicates
+#Data of Tuple is Immutable
+#Tuple itself is Immutable
+
+
+data = (10,20,30,40,"Marvellous",10);
+
+print("Data type is :",type(data));
+
+print("Elements are :",data);
+
+print("0th element is :",data[0]);
+
+print("Element at 3rd index :",data[3]);
+
+print("Last element :",data[-1]);
+
+#data[0] = 11;              #Not Allowed i.e. immutable
+
+#data.append(50);
+
+#print("Updated data is :",data);
+
