14da12f430ec92f4702e53ceac5ce2d747e5e323
unknown <pranalimohite482000@gmail.com>
2022-01-24 18:28:52 +0530

    Python Program

---
+++ Python Programs/Set/03_Set.py
@@ -0,0 +1,20 @@
+print("Enter number of elements");
+
+size =int(input());
+
+data = set();
+
+for i in range(size):
+    print("Enter element no :",i+1);
+    no = int(input());
+    data.add(no);
+    
+print("Data from set is :",data);
+
+print("Enter data to search");
+value = int(input());
+
+if value in data:
+    print("Element is there");
+else:
+    print("There is no such element");
