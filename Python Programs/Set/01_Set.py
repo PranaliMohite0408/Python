6dac3977fb4af0df98cfc607f17e08a20afb3f90
unknown <pranalimohite482000@gmail.com>
2022-01-24 18:27:38 +0530

    Python Program

---
+++ Python Programs/Set/01_Set.py
@@ -0,0 +1,27 @@
+data = {10,20,30,40}
+
+MyList = [10,20,30,40];
+
+print("Datatype is :",type(data));
+
+print("Length is :",len(data));
+
+print("Data from set :",data);
+
+print("Data from list :",MyList);
+
+
+#print(data[0]);
+
+print("Data traversal using loop");
+for No in data:
+    print(No);
+    
+    
+data1 = {10,20,30,40,10}        #Duplicate elements allows but it stores only one time
+
+print("New set is :",data1);   
+
+data2 = {True,10,20,30.5,"Marvellous"};
+
+print(data2);
