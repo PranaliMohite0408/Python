45a3c2cef1cadd722198dd30fc5f1029b1ba6842
unknown <pranalimohite482000@gmail.com>
2022-02-05 21:28:22 +0530

    Python Program

---
+++ Python Programs/Range/01_Range.py
@@ -0,0 +1,20 @@
+#print(range(5));
+
+#No = range(5);
+
+#print(No);
+
+#for i in range(5):
+#   print(i);
+
+#for i in range(0,5):
+#    print(i);
+    
+for i in range(0,5,1):
+    print(i);
+    
+#range(start, end, displacement)
+
+#range(0,5,1)
+#range(0,5)
+#range(5)
