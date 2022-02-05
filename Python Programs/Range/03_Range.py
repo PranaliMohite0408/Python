718f45dde7c64d6b9640c07bcedf89e57b4149cd
unknown <pranalimohite482000@gmail.com>
2022-02-05 21:30:28 +0530

    Python Program

---
+++ Python Programs/Range/03_Range.py
@@ -0,0 +1,13 @@
+def Iteration(value):
+    for i in range(value):
+        print("Hello");
+
+def main():
+    print("Enter the frequency");
+    
+    No = int(input());
+    
+    Iteration(No);
+
+if __name__ == "__main__":
+    main();
