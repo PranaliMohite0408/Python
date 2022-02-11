86a835217c4497d76d4361659c7c63d4dc3d8310
unknown <pranalimohite482000@gmail.com>
2022-02-11 17:41:43 +0530

    Python Program

---
+++ Python Programs/Iteration/02_Iteration.py
@@ -0,0 +1,15 @@
+#We have to display 5 times "Hello" on screen
+
+def Iteration():
+    No = 0
+    
+    while(No < 5):
+        print("Hello")
+        No = No + 1
+
+def main():
+    Iteration()
+
+
+if __name__ == "__main__":
+    main()
