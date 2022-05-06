d177766fa03a235b2b4e5d496c6b42d087caa07f
Pranali Mohite <pranalimohite482000@gmail.com>
2022-05-06 15:05:15 +0530

    Python Program

---
+++ Automation/Command Line Argument/01_Command.py
@@ -0,0 +1,14 @@
+from sys import *
+
+def main():
+    print("Number of Command line arguments are :",len(argv))
+
+    print("Name of Application is :",argv[0])
+
+    print("First argument :",argv[1])
+
+    print("Second Argument :",argv[2])
+
+
+if __name__ == "__main__":
+    main()
