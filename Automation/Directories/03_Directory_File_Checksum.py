631c766a3efb48cb4b7dff8db4401d29f121fe51
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-11-08 21:21:27 +0530

    Automation Application

---
+++ Automation/Directories/03_Directory_File_Checksum.py
@@ -0,0 +1,69 @@
+from sys import *
+import os
+import hashlib
+
+
+def hashfile(path, blocksize = 1024):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    
+    return hasher.hexdigest()
+
+
+def DisplayChecksum(path):
+    flag = os.path.isabs(path)
+
+    if flag == False:
+        path = os.path.abspath(path)
+
+    exists = os.path.isdir(path)
+
+    if exists:
+        for dirName, subdirs, fileList in os.walk(path):
+            print("Current Folder is "+dirName)
+
+            for filen in fileList:
+                path = os.path.join(dirName, filen)
+                file_hash = hashfile(path)
+                print(path)
+                print(file_hash)
+                print(' ')
+    else:
+        print("Invalid Path")
+
+
+def main():
+    print("---------- Marvellous Infosystems ----------")
+
+    print("Application Name : "+argv[0])
+
+    if (len(argv) != 2):
+        print("Error : Invalid Number of Arguments")
+        exit()
+
+    if (argv[1] == "-h") or (argv[1] == "-H"):
+        print("This script is used to traverse specific directory and display Checksum of files")
+        exit()
+
+    if (argv[1] == "-u") or (argv[1] == "-U"):
+        print("Usage : ApplicationName AbsolutePath_of_Directory ")
+        exit()
+
+    try:
+        arr = DisplayChecksum(argv[1])
+
+    except ValueError:
+        print("Error : Invalid datatype of Input")
+
+    except Exception as E:
+        print("Error: Invalid Input",E)
+
+
+if __name__ == "__main__":
+    main()
